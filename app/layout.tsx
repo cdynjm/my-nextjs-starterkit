0689f9386721d16c1d03d512ccbba80a74ffffb2
cdynjm <jemuelcadayona3@gmail.com>
2025-06-15 17:08:23 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Providers } from "../providers/providers";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../lib/auth";
 import { NProgressProvider } from "@/providers/NProgressProvider";
-import { AuthenticatedLayout } from "./authenticatedLayout";
+import { AuthenticatedLayout } from "./AuthenticatedLayout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
